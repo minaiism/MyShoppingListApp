8d31abb074fe3378f0fd7a721087823692e8bbd0
Martyna Wójcik <martyna.wojcik.mail@gmail.com>
2018-04-30 02:11:56 +0200

    delete comments

--- src/components/Autocomplete/Autocomplete.js
+++ src/components/Autocomplete/Autocomplete.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Autosuggest from 'react-autosuggest';
 import data from './food.json';
 
-// Teach Autosuggest how to calculate suggestions for any given input value.
 const getSuggestions = (value, items) => {
     const inputValue = value.trim().toLowerCase();
     const inputLength = inputValue.length;
@@ -12,12 +11,8 @@ const getSuggestions = (value, items) => {
     );
 };
 
-// When suggestion is clicked, Autosuggest needs to populate the input
-// based on the clicked suggestion. Teach Autosuggest how to calculate the
-// input value for every given suggestion.
 const getSuggestionValue = suggestion => suggestion.name;
 
-// Use your imagination to render suggestions.
 const renderSuggestion = suggestion => (
     <div className="autoCompleteText">
         {suggestion.name}
@@ -28,11 +23,6 @@ class Autocomplete extends React.Component {
     constructor() {
         super();
 
-        // Autosuggest is a controlled component.
-        // This means that you need to provide an input value
-        // and an onChange handler that updates this value (see below).
-        // Suggestions also need to be provided to the Autosuggest,
-        // and they are initially empty because the Autosuggest is closed.
         this.state = {
             value: '',
             suggestions: [],
@@ -48,15 +38,12 @@ class Autocomplete extends React.Component {
         this.props.onChange("newItem", newValue)
     };
 
-    // Autosuggest will call this function every time you need to update suggestions.
-    // You already implemented this logic above, so just use it.
     onSuggestionsFetchRequested = ({value}) => {
         this.setState({
             suggestions: getSuggestions(value, this.state.foodFromJson)
         });
     };
 
-    // Autosuggest will call this function every time you need to clear suggestions.
     onSuggestionsClearRequested = () => {
         this.setState({
             suggestions: []
@@ -72,7 +59,6 @@ class Autocomplete extends React.Component {
     render() {
         const {value, suggestions} = this.state;
 
-        // Autosuggest will pass through all these props to the input.
         const inputProps = {
             placeholder: this.props.placeholder,
             value: this.props.value,
@@ -80,7 +66,6 @@ class Autocomplete extends React.Component {
             onKeyUp: this.props.onKeyUp
         };
 
-        // Finally, render it!
         return (
             <Autosuggest
                 suggestions={suggestions}
